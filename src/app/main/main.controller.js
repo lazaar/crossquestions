16b8a57dde562ca50ecc7c5234520037cf637fe9
ilazaar <ilazaar@sqli.com>
2017-07-27 22:40:33 +0100

    Question design

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -4,15 +4,16 @@
     angular
         .module('crossQuestions')
         //Controller of the main page
-        .controller('MainController', function (routerHelper, dataService) {
+        .controller('MainController', function (routerHelper, dataService, $ionicHistory) {
 
             var vm = this;
-
+            vm.goBack = function (){
+                $ionicHistory.goBack(); 
+            };
 
 
             function init(){
                 vm.goToState = routerHelper.goToState;
-
                 //Init Data
                 dataService.getData();
             }
